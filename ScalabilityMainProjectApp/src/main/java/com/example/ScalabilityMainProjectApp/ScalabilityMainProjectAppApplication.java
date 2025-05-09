f69643c46c83d2bcf90b630b606c5ff2fe6e627e
KareemELadl <84328201+KareemELadl@users.noreply.github.com>
2025-05-09 17:19:39 +0300

    Project Init

---
+++ ScalabilityMainProjectApp/src/main/java/com/example/ScalabilityMainProjectApp/ScalabilityMainProjectAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ScalabilityMainProjectApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalabilityMainProjectAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalabilityMainProjectAppApplication.class, args);
+	}
+
+}
---
+++ ScalabilityMainProjectApp/src/test/java/com/example/ScalabilityMainProjectApp/ScalabilityMainProjectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ScalabilityMainProjectApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalabilityMainProjectAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
